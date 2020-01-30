fa4751f95ac294ee990ff97c739e6a05a3797971
Daniel Rosenberg <drosen@google.com>
2020-01-29 19:40:07 -0800

    Mi8937: Enable Project ID Quota support on userdata
    These features enable wide inodes on userdata, which is required for
    project id quotas. Wide inodes can only be set at mkfs time.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67

--- device.mk
+++ device.mk
@@ -58,6 +58,11 @@ PRODUCT_PACKAGES += \
     tune2fs_ramdisk \
     resize2fs_ramdisk
 
+# Enable wider inodes for project quotas
+# Later, replace with $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+PRODUCT_QUOTA_PROJID := 1
+PRODUCT_PRODUCT_PROPERTIES += ro.emulated_storage.projid=1
+
 # Fingerprint
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/blankfile:$(TARGET_COPY_OUT_ODM)/bin/gx_fpd
