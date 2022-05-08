b52a6981dfb9e7bf3bed6aeb5e3642d968ddda3f
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-09 04:27:55 +0800

    Mi8937: CipherOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_Mi8937-user \
     lineage_Mi8937-userdebug \
     lineage_Mi8937-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/cipher_Mi8937.mk
+
+COMMON_LUNCH_CHOICES += \
+    cipher_Mi8937-user \
+    cipher_Mi8937-userdebug \
+    cipher_Mi8937-eng
