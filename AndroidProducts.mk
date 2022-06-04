2be23dc66015157258a39dc85b469c23be363208
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-04 20:17:08 +0800

    Mi8937: RevengeOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_Mi8937-user \
     lineage_Mi8937-userdebug \
     lineage_Mi8937-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_Mi8937.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_Mi8937-user \
+    revengeos_Mi8937-userdebug \
+    revengeos_Mi8937-eng
