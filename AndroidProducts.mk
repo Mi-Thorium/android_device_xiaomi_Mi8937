42d684213db1042a7c23d5e3449925937fe10636
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-13 04:18:57 +0800

    Mi8937: AwakenOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_Mi8937-user \
     lineage_Mi8937-userdebug \
     lineage_Mi8937-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/awaken_Mi8937.mk
+
+COMMON_LUNCH_CHOICES += \
+    awaken_Mi8937-user \
+    awaken_Mi8937-userdebug \
+    awaken_Mi8937-eng
---
+++ awaken_Mi8937.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2021 The AwakenOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Inherit some common AwakenOS stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+
+# Inherit from Mi8937 device
+ROM := awaken
+$(call inherit-product, device/xiaomi/Mi8937/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Mi8937
+PRODUCT_NAME := awaken_Mi8937
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MSM8937
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V10.2.2.0.MALMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/land/land:6.0.1/MMB29M/V10.2.2.0.MALMIXM:user/release-keys"
+
+# Awaken
+TARGET_BOOT_ANIMATION_RES := 720
