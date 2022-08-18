9839cfebcd730f846312028a0b7857355a5ff4c8
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-18 20:34:26 +0800

    Mi8937: Add AOSP product makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_Mi8937-user \
     lineage_Mi8937-userdebug \
     lineage_Mi8937-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/aosp_Mi8937.mk
+
+COMMON_LUNCH_CHOICES += \
+    aosp_Mi8937-user \
+    aosp_Mi8937-userdebug \
+    aosp_Mi8937-eng
