24c19db6a9a09f6c8ea6338e1c06eb7e5dbc303a
me-cafebabe <me.cafebabe@gmail.com>
2023-01-29 16:10:51 +0800

    Revert "Mi8937: powershare: Abuse powershare HAL for flashlight on prada"
    This reverts commit bb6af0c72ac6c321ad43d905e5e90fb6d154724b.

--- powershare/service.cpp
+++ powershare/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.powershare@1.0-service.xiaomi_prada_flashlight"
+#define LOG_TAG "vendor.lineage.powershare@1.0-service.oneplus"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
