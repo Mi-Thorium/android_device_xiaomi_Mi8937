7d56bae726c71a397b3f9e609f56edfc1c6945d3
me-cafebabe <me.cafebabe@gmail.com>
2023-02-07 15:17:06 +0800

    Mi8937: Add `Mi8937_1GB_RAM` target for imitating Redmi Go
    I doesn't own Redmi Go, but I have to troubleshoot Low RAM issues on it sometimes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,12 +6,16 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_Mi8937.mk \
+    $(LOCAL_DIR)/lineage_Mi8937_1GB_RAM.mk \
     $(LOCAL_DIR)/lineage_Mi8937_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_Mi8937-user \
     lineage_Mi8937-userdebug \
     lineage_Mi8937-eng \
+    lineage_Mi8937_1GB_RAM-user \
+    lineage_Mi8937_1GB_RAM-userdebug \
+    lineage_Mi8937_1GB_RAM-eng \
     lineage_Mi8937_4_19-user \
     lineage_Mi8937_4_19-userdebug \
     lineage_Mi8937_4_19-eng
---
+++ lineage_Mi8937_1GB_RAM.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Kernel
+TARGET_KERNEL_VERSION := 4.9
+
+# Inherit from Mi8937 device
+MI8937_LIMIT_RAM_TO_1GB := true
+$(call inherit-product, device/xiaomi/Mi8937/device.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    xiaomi_landtoni_overlay_lineage \
+    xiaomi_prada_overlay_lineage \
+    xiaomi_riva_overlay_lineage \
+    xiaomi_rolex_overlay_lineage \
+    xiaomi_ulysse_overlay_lineage
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Mi8937_1GB_RAM
+PRODUCT_NAME := lineage_Mi8937_1GB_RAM
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MSM8937
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V10.2.2.0.MALMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/land/land:6.0.1/MMB29M/V10.2.2.0.MALMIXM:user/release-keys"
