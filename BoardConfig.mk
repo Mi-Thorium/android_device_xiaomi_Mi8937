ac3b93b17569970753da5478a866d470d3284e4f
me-cafebabe <me.cafebabe@gmail.com>
2023-05-15 01:07:06 +0800

    Mi8937: Enable UClamp and exFAT in kernel
    Change-Id: Ic6e5af28bb42e89b8fe56e1a3a28e41b6d0fdab6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,9 @@ TARGET_KERNEL_CONFIG := \
     vendor/xiaomi/msm8937/common.config \
     vendor/xiaomi/msm8937/mi8937.config \
     vendor/xiaomi/feature/android-12.config \
-    vendor/xiaomi/feature/lmkd.config
+    vendor/xiaomi/feature/exfat.config \
+    vendor/xiaomi/feature/lmkd.config \
+    vendor/xiaomi/feature/uclamp.config
 
 ifeq ($(MI8937_CAM_USE_LATEST_CAMERA_STACK),true)
 TARGET_KERNEL_CONFIG += vendor/xiaomi/msm8937/feature/latest-camera-stack.config
