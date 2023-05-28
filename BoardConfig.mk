cfe7a992fe3a069b8cb2e0b645e58d1377b8a477
me-cafebabe <me.cafebabe@gmail.com>
2023-05-28 23:19:49 +0800

    Mi8937: Disable Camera HAL1 on kernel 4.19
    * It's bugged, even with legacy camera stack
    
    Change-Id: I22359871f027dc62ef7b4c114e5ec9e37862ffcd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,9 @@ TARGET_OTA_ASSERT_DEVICE := mi8937,landtoni,land,santoni,prada,ulova,ulysse,uggl
 MI8937_CAM_USE_RENAMED_BLOBS_L := true
 MI8937_CAM_USE_RENAMED_BLOBS_P := true
 MI8937_CAM_USE_RENAMED_BLOBS_U := true
+ifeq ($(TARGET_KERNEL_VERSION),4.19)
+TARGET_SUPPORT_HAL1 := false
+endif
 
 # Display
 TARGET_SCREEN_DENSITY := 280
