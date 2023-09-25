5b2af92b52b5e2da7c90c82cf2067635b3cb5953
me-cafebabe <me.cafebabe@gmail.com>
2023-09-25 15:05:45 +0800

    Mi8937: Convert product makefile to AOSP
    Change-Id: Ie7e9f861c130623330698a4b2234929c08a01614

--- aosp_Mi8937_4_19.mk
+++ aosp_Mi8937_4_19.mk
@@ -4,34 +4,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
 
 # Inherit from Mi8937 device
 $(call inherit-product, device/xiaomi/Mi8937/device.mk)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_PACKAGES += \
-    xiaomi_prada_overlay_lineage \
-    xiaomi_riva_overlay_lineage \
-    xiaomi_rolex_overlay_lineage \
-    xiaomi_ulysse_overlay_lineage \
-    xiaomi_wt8937_overlay_lineage
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi8937_4_19
-PRODUCT_NAME := lineage_Mi8937_4_19
+PRODUCT_NAME := aosp_Mi8937_4_19
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MSM8937
