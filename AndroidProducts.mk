0eb1c0cd16eceb980482728b075a71af1b92907f
me-cafebabe <me.cafebabe@gmail.com>
2023-09-25 15:05:45 +0800

    Mi8937: Convert product makefile to AOSP
    Change-Id: Ie7e9f861c130623330698a4b2234929c08a01614

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi8937.mk \
-    $(LOCAL_DIR)/lineage_Mi8937_4_19.mk
+    $(LOCAL_DIR)/aosp_Mi8937.mk \
+    $(LOCAL_DIR)/aosp_Mi8937_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi8937-user \
-    lineage_Mi8937-userdebug \
-    lineage_Mi8937-eng \
-    lineage_Mi8937_4_19-user \
-    lineage_Mi8937_4_19-userdebug \
-    lineage_Mi8937_4_19-eng
+    aosp_Mi8937-user \
+    aosp_Mi8937-userdebug \
+    aosp_Mi8937-eng \
+    aosp_Mi8937_4_19-user \
+    aosp_Mi8937_4_19-userdebug \
+    aosp_Mi8937_4_19-eng
