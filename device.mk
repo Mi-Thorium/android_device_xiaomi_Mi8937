dbe1e4d19e2ab49e34d5e0ab6bf6613d07ec3032
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-16 15:05:03 +0800

    Mi8937: Switch to AIDL LiveDisplay sysfs HAL
    Change-Id: I2ab593c86e7e1cd8139d918ba4b3488d90965006

--- device.mk
+++ device.mk
@@ -92,7 +92,7 @@ PRODUCT_COPY_FILES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+    vendor.lineage.livedisplay-service.sysfs
 
 # Placeholder
 PRODUCT_COPY_FILES += \
